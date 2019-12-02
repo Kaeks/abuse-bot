da957f815edba517aba06092e32f6a9ae7b7a41e
Kaeks <jakey.jjm@gmail.com>
2019-12-02 16:24:41 +0100

    Add handling for already existing command names

--- commands/custom.js
+++ commands/custom.js
@@ -12,12 +12,54 @@ const { argumentValues, permissionLevels, colors } = enums;
 let commandCustomAdd = new SubCommand('add', argumentValues.REQUIRED)
 	.addDoc('<name> <function>', 'Add a custom function.')
 	.setExecute((msg, suffix) => {
+		let user = msg.author;
 		let name = suffix.split(/ +/)[0];
 		let temp = suffix.substring(name.length);
 		let match = temp.match(/ +/);
 		let fn = match !== null ? temp.substring(match[0].length) : null;
-		let customFunction = new CustomFunction(name, fn, msg.author);
-		common.addCustomFunction(customFunction);
+		if (common.customFunctions.has(name)) {
+			const CONFIRMATION_TIMEOUT = 30;
+			let existsEmbed = new Discord.RichEmbed()
+				.setColor(colors.PURPLE)
+				.setTitle('Already exists!')
+				.setDescription(
+					'A custom command with the name `' + name + '` already exists.' + '\n'
+					+ 'Do you want to replace it?'
+				).setFooter('React with ' + common.PREF_CONFIRMATION_EMOJI_BASE + ' to confirm (' + CONFIRMATION_TIMEOUT + ' seconds)');
+			msg.channel.send({ embed: existsEmbed }).then(message => {
+				message.react(common.PREF_CONFIRMATION_EMOJI_BASE);
+				message.awaitReactions((reaction, reactor) => {
+					return Object.values(enums.confirmationEmojis).includes(reaction.emoji.name) && reactor === user
+				}, {
+					time : CONFIRMATION_TIMEOUT * enums.timeSpans.SECOND,
+					max : 1,
+					errors : ['time']
+				}).then(collected => {
+					createCustomFunctionEntry(msg, name, fn, user);
+					let reactionEmojiName = collected.first().emoji.name;
+					let reactionEmojiBase = reactionEmojiName.substring(0,2);
+					let friendlyDescription = common.PREF_CONFIRMATION_EMOJI_BASE + ' Replaced the custom function `' + name + '`.';
+					let rudeDescription = 'Listen up, kid. You were supposed to react with ' + common.PREF_CONFIRMATION_EMOJI_BASE + ' and not '
+						+ reactionEmojiName + '.' + '\n'
+						+ 'I\'ll let that slip this time and replaced the custom function `' + name + '`.';
+					let successEmbed = new Discord.RichEmbed()
+						.setColor(colors.GREEN)
+						.setTitle('Replaced custom function!')
+						.setDescription(reactionEmojiBase === common.PREF_CONFIRMATION_EMOJI_BASE ? friendlyDescription : rudeDescription);
+					message.edit({ embed: successEmbed });
+				}).catch(() => {
+					let abortedEmbed = new Discord.RichEmbed()
+						.setColor(colors.RED)
+						.setTitle('Confirmation timed out!')
+						.setDescription('You did not confirm your action. Process aborted.');
+					message.edit({ embed: abortedEmbed }).then(editedMessage => {
+						editedMessage.delete(5000);
+					});
+				});
+			});
+		} else {
+			createCustomFunctionEntry(msg, name, fn, user);
+		}
 	});
 
 let commandCustomList = new SubCommand('list', argumentValues.NONE)
@@ -84,3 +126,13 @@ let commandCustom = new Command('custom', argumentValues.NULL, permissionLevels.
 	.addSub(commandCustomExecute);
 
 module.exports = commandCustom;
+
+function createCustomFunctionEntry(msg, name, fn, creator) {
+	let customFunction = new CustomFunction(name, fn, creator);
+	common.addCustomFunction(customFunction);
+	let createdEmbed = new Discord.RichEmbed()
+		.setColor(colors.PRESTIGE)
+		.setTitle('Created custom function!')
+		.setDescription('Created the custom function `' + name + '`.' + '\n' + '>>> ' + fn);
+	msg.channel.send({ embed: createdEmbed });
+}
